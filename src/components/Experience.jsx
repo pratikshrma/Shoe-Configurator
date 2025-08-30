c8651559609c968739b612701a46ae45230db2ea
Pratik <amnshrma341@gmail.com>
2025-08-30 16:33:39 +0530

    fixed some  more things

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react'
 import * as THREE from 'three'
-import { useGLTF, Float, ContactShadows, PresentationControls, Bvh, checkIfFrameIsEmpty } from '@react-three/drei'
+import { useGLTF, Float, ContactShadows, PresentationControls, Bvh, checkIfFrameIsEmpty, OrbitControls } from '@react-three/drei'
 import { useStore } from '../stores/global'
 import { useFrame, useThree } from '@react-three/fiber'
 
@@ -57,26 +57,22 @@ const Experience = () => {
 
     return (
         <>
-            <PresentationControls
-                global // controls the entire scene instead of a local group
-                polar={[-0.4, 0.8]} // vertical rotation limits (up/down)
-                speed={2.2}
-            >
-                <ResponsiveModel>
-                    <Float>
-                        <group ref={shoeRef} dispose={null} scale={[2, 2, 2]} onPointerOut={() => setHoverMaterialKey(null)}>
-                            <mesh onPointerUp={(e) => setMaterialKey(e, "laces")} castShadow receiveShadow geometry={model.nodes.shoe.geometry} material={model.materials.laces} />
-                            <mesh onPointerUp={(e) => setMaterialKey(e, "mesh")} castShadow receiveShadow geometry={model.nodes.shoe_1.geometry} material={model.materials.mesh} />
-                            <mesh onPointerUp={(e) => setMaterialKey(e, "caps")} castShadow receiveShadow geometry={model.nodes.shoe_2.geometry} material={model.materials.caps} />
-                            <mesh onPointerUp={(e) => setMaterialKey(e, "inner")} castShadow receiveShadow geometry={model.nodes.shoe_3.geometry} material={model.materials.inner} />
-                            <mesh onPointerUp={(e) => setMaterialKey(e, "sole")} castShadow receiveShadow geometry={model.nodes.shoe_4.geometry} material={model.materials.sole} />
-                            <mesh onPointerUp={(e) => setMaterialKey(e, "stripes")} castShadow receiveShadow geometry={model.nodes.shoe_5.geometry} material={model.materials.stripes} />
-                            <mesh onPointerUp={(e) => setMaterialKey(e, "band")} castShadow receiveShadow geometry={model.nodes.shoe_6.geometry} material={model.materials.band} />
-                            <mesh onPointerUp={(e) => setMaterialKey(e, "patch")} castShadow receiveShadow geometry={model.nodes.shoe_7.geometry} material={model.materials.patch} />
-                        </group>
-                    </Float>
-                </ResponsiveModel>
-            </PresentationControls>
+            <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={true} enablePan={false} />
+
+            <ResponsiveModel>
+                <Float>
+                    <group ref={shoeRef} dispose={null} scale={[2, 2, 2]} onPointerOut={() => setHoverMaterialKey(null)}>
+                        <mesh onPointerUp={(e) => setMaterialKey(e, "laces")} castShadow receiveShadow geometry={model.nodes.shoe.geometry} material={model.materials.laces} />
+                        <mesh onPointerUp={(e) => setMaterialKey(e, "mesh")} castShadow receiveShadow geometry={model.nodes.shoe_1.geometry} material={model.materials.mesh} />
+                        <mesh onPointerUp={(e) => setMaterialKey(e, "caps")} castShadow receiveShadow geometry={model.nodes.shoe_2.geometry} material={model.materials.caps} />
+                        <mesh onPointerUp={(e) => setMaterialKey(e, "inner")} castShadow receiveShadow geometry={model.nodes.shoe_3.geometry} material={model.materials.inner} />
+                        <mesh onPointerUp={(e) => setMaterialKey(e, "sole")} castShadow receiveShadow geometry={model.nodes.shoe_4.geometry} material={model.materials.sole} />
+                        <mesh onPointerUp={(e) => setMaterialKey(e, "stripes")} castShadow receiveShadow geometry={model.nodes.shoe_5.geometry} material={model.materials.stripes} />
+                        <mesh onPointerUp={(e) => setMaterialKey(e, "band")} castShadow receiveShadow geometry={model.nodes.shoe_6.geometry} material={model.materials.band} />
+                        <mesh onPointerUp={(e) => setMaterialKey(e, "patch")} castShadow receiveShadow geometry={model.nodes.shoe_7.geometry} material={model.materials.patch} />
+                    </group>
+                </Float>
+            </ResponsiveModel>
             <ContactShadows global opacity={1} scale={10} blur={5} position={[0, -1.0, 0]} resolution={256} color={'#dbc9c9'} />
         </>
     )
